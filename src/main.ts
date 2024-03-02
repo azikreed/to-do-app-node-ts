2de767c156b84f61abc88b2fbcd2998de9ca7b31
azikreed <azikreed@gmail.com>
2024-03-02 16:35:16 +0300

    [+] added mongo service

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,13 @@ import { ExceptionFilter } from './helpers/errors/exception.filter';
 import { App } from './app';
 import { IConfigService } from './interfaces/config.interface';
 import { ConfigService } from './helpers/config/config.service';
+import { MongoService } from './services/db.service';
 
 const appBindings = new ContainerModule((bind: interfaces.Bind) => {
 	bind<ILogger>(TYPES.LoggerService).to(LoggerService).inSingletonScope();
 	bind<IExceptionFilter>(TYPES.ExceptionFilter).to(ExceptionFilter);
 	bind<IConfigService>(TYPES.ConfigService).to(ConfigService).inSingletonScope();
+	bind<MongoService>(TYPES.MongoService).to(MongoService).inSingletonScope();
 	bind<App>(TYPES.Application).to(App);
 });
 
