af6f84d0ff2a2856158e0ed0df83495147e6e2a1
azikreed <azikreed@gmail.com>
2024-03-02 19:51:53 +0300

    [+] UserRepository added

---
+++ src/repositories/user.repository.ts
@@ -0,0 +1,25 @@
+import { inject, injectable } from 'inversify';
+import { IUserRepository } from '../interfaces/user.repository.interface';
+import { TYPES } from '../types';
+import { MongoService } from '../services/db.service';
+import { User } from '../entities/user.entity';
+import { IUserModel } from '../interfaces/user.model.interface';
+import { UserModel } from '../models/User/User';
+
+@injectable()
+export class UserRepository implements IUserRepository {
+	constructor() {}
+
+	async create({ email, password, name }: User): Promise<IUserModel> {
+		const user = new UserModel({
+			name,
+			email,
+			password,
+		});
+		return await user.save();
+	}
+
+	async find(email: string): Promise<IUserModel | null> {
+		return await UserModel.findOne({ email: email });
+	}
+}
