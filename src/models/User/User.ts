dc51e3b9e3699b59ab3c78cfc0cad57e07b35a67
azikreed <azikreed@gmail.com>
2024-03-02 22:13:35 +0300

    [*] fixed - name field added to schema of user

--- src/models/User/User.ts
+++ src/models/User/User.ts
@@ -4,6 +4,7 @@ import { IUserModel } from '../../interfaces/user.model.interface';
 const userSchema = new Schema({
 	email: { type: String, required: true, unique: true },
 	password: { type: String, required: true },
+	name: { type: String, required: true },
 	createdAt: { type: Date, default: Date.now },
 	updatedAt: { type: Date, default: Date.now },
 });
