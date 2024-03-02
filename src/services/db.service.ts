0b31fc96429a0e1873e268ca3957bf310ce231f2
azikreed <azikreed@gmail.com>
2024-03-02 19:52:29 +0300

    [*] mongodb deleted from code

--- src/services/db.service.ts
+++ src/services/db.service.ts
@@ -2,23 +2,19 @@ import { inject, injectable } from 'inversify';
 import mongoose from 'mongoose';
 import { ILogger } from '../interfaces/logger.service.interface';
 import { TYPES } from '../types';
-import { MongoClient } from 'mongodb';
 import { IConfigService } from '../interfaces/config.interface';
 
 @injectable()
 export class MongoService {
-	client: MongoClient;
-
 	constructor(
 		@inject(TYPES.LoggerService) private logger: ILogger,
 		@inject(TYPES.ConfigService) private configService: IConfigService,
-	) {
-		this.client = new MongoClient(this.configService.get('MONGO_URI'));
-	}
+	) {}
 
 	async connect(): Promise<void> {
 		try {
-			await this.client.connect();
+			const url = this.configService.get('MONGO_URI');
+			await mongoose.connect(url);
 			this.logger.log('[MongoService] Успешно подключились к базе данных');
 		} catch (e) {
 			if (e instanceof Error) {
@@ -28,6 +24,6 @@ export class MongoService {
 	}
 
 	async disconnect(): Promise<void> {
-		await this.client.close();
+		await mongoose.disconnect();
 	}
 }
