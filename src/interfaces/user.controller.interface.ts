f81d415796d10e9767c228bb7157033dc8f1ac23
azikreed <azikreed@gmail.com>
2024-03-02 19:46:28 +0300

    [+] info controller fixed

--- src/interfaces/user.controller.interface.ts
+++ src/interfaces/user.controller.interface.ts
@@ -3,5 +3,4 @@ import { NextFunction, Request, Response } from 'express';
 export interface IUserController {
 	login: (req: Request, res: Response, next: NextFunction) => void;
 	register: (req: Request, res: Response, next: NextFunction) => void;
-	info: (req: Request, res: Response, next: NextFunction) => void;
 }
