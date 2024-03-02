ede21de81ffb7dd1b080a5ee4620494cdb657202
azikreed <azikreed@gmail.com>
2024-03-02 16:39:07 +0300

    [+] added user service

---
+++ src/services/user.service.ts
@@ -0,0 +1,36 @@
+import { inject } from 'inversify';
+import { IUserService } from '../interfaces/user.service.interface';
+import { TYPES } from '../types';
+import { IConfigService } from '../interfaces/config.interface';
+import { UserRegisterDto } from './dto/user-register.dto';
+import { IUserModel } from '../interfaces/user.model.interface';
+import { User } from '../entities/user.entity';
+import { IUserRepository } from '../interfaces/user.repository.interface';
+import { UserLoginDto } from './dto/user-login.dto';
+
+export class UserService implements IUserService {
+	constructor(
+		@inject(TYPES.ConfigService) private configService: IConfigService,
+		@inject(TYPES.UserRepository) private userRepository: IUserRepository,
+	) {}
+
+	async createUser({ email, name, password }: UserRegisterDto): Promise<IUserModel | null> {
+		const newUser = new User(email, name);
+		const salt = Number(this.configService.get('SALT'));
+		await newUser.setPassword(password, salt);
+		const existedUser = await this.userRepository.find(email);
+		if (existedUser) {
+			return null;
+		}
+		return this.userRepository.create(newUser);
+	}
+
+	async validateUser({ email, password }: UserLoginDto): Promise<boolean> {
+		const existedUser = await this.userRepository.find(email);
+		if (!existedUser) {
+			return false;
+		}
+		const newUser = new User(existedUser?.email, existedUser?.name, existedUser.password);
+		return newUser.comparePassword(password);
+	}
+}
