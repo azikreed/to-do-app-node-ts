7930b525eaef73033491d7d4f6e97d29b4f95979
azikreed <azikreed@gmail.com>
2024-03-02 19:58:55 +0300

    [*] deleted field "id"

--- src/models/User/User.ts
+++ src/models/User/User.ts
@@ -3,7 +3,6 @@ import { v4 as uuidv4 } from 'uuid';
 import { IUserModel } from '../../interfaces/user.model.interface';
 
 const userSchema = new Schema({
-	id: { type: String, default: uuidv4, required: true, unique: true },
 	email: { type: String, required: true, unique: true },
 	password: { type: String, required: true },
 	createdAt: { type: Date, default: Date.now },
