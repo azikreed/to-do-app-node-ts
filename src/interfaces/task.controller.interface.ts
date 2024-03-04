585826991c2b8aa4591ea8294205f9eefa2be79c
azikreed <azikreed@gmail.com>
2024-03-04 13:49:38 +0300

    [+] added update method

--- src/interfaces/task.controller.interface.ts
+++ src/interfaces/task.controller.interface.ts
@@ -3,4 +3,5 @@ import { NextFunction, Request, Response } from 'express';
 export interface ITaskController {
 	create: (req: Request, res: Response, next: NextFunction) => void;
 	getAll: (req: Request, res: Response, next: NextFunction) => void;
+	update: (req: Request, res: Response, next: NextFunction) => void;
 }
