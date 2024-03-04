23654f8554262b52231266ef14e6c7926a24b10e
azikreed <azikreed@gmail.com>
2024-03-04 19:25:00 +0300

    [+] added (in comment) testing for update, delete, getone. I have to fix it as soon as possible

--- tests/task.e2e-spec.ts
+++ tests/task.e2e-spec.ts
@@ -63,6 +63,69 @@ describe('Task e2e', () => {
 
 		expect(res.statusCode).toBe(401);
 	});
+
+	// it('getOne - success', async () => {
+	// 	const login = await request(application.app)
+	// 		.post('/user/login')
+	// 		.send({ email: 'azikreed@gmail.com', password: '12345678' });
+	// 	const getAll = await request(application.app)
+	// 		.get('/task/get')
+	// 		.set('Authorization', `Bearer ${login.body.token}`);
+	// 	const res = await request(application.app)
+	// 		.get(`/task/get/65e4691401730a4a114d5a82`)
+	// 		.set('Authorization', `Bearer ${login.body.token}`);
+
+	// 	expect(res.statusCode).toBe(200);
+	// });
+
+	// it('getOne - error', async () => {
+	// 	const login = await request(application.app)
+	// 		.post('/user/login')
+	// 		.send({ email: 'azikreed@gmail.com', password: '12345678' });
+	// 	const res = await request(application.app)
+	// 		.get(`/task/get/65e4691401730a4a114d5a8`)
+	// 		.set('Authorization', `Bearer ${login.body.token}`);
+
+	// 	expect(res.statusCode).toBe(400);
+	// });
+
+	// it('update - success', async () => {
+	// 	const login = await request(application.app)
+	// 		.post('/user/login')
+	// 		.send({ email: 'azikreed@gmail.com', password: '12345678' });
+	// 	const res = await request(application.app)
+	// 		.patch('/task/update/65e4691401730a4a114d5a82')
+	// 		.send({ title: 'not any title not not', updatedAt: new Date() })
+	// 		.set('Authorization', `Bearer ${login.body.token}`);
+
+	// 	expect(res.statusCode).toBe(200);
+	// });
+
+	// it('update - error', async () => {
+	// 	const login = await request(application.app)
+	// 		.post('/user/login')
+	// 		.send({ email: 'azikreed@gmail.com', password: '12345678' });
+	// 	const res = await request(application.app)
+	// 		.patch('/task/update/65e4691401730a4a114d5a81')
+	// 		.send({ title: 'not any title not not', updatedAt: new Date() })
+	// 		.set('Authorization', `Bearer ${login.body.token}`);
+
+	// 	expect(res.statusCode).toBe(400);
+	// });
+
+	// it('delete - success', async () => {
+	// 	const login = await request(application.app)
+	// 		.post('/user/login')
+	// 		.send({ email: 'azikreed@gmail.com', password: '12345678' });
+	// 	const getAll = await request(application.app)
+	// 		.get('/task/get')
+	// 		.set('Authorization', `Bearer ${login.body.token}`);
+	// 	const res = await request(application.app)
+	// 		.patch('/task/delete/65e4691401730a4a114d5a82')
+	// 		.set('Authorization', `Bearer ${login.body.token}`);
+
+	// 	expect(res.statusCode).toBe(200);
+	// });
 });
 
 afterAll(() => {
