ad2b9bba0a783e90ac7da10a2bd69a4e96de7a1e
azikreed <azikreed@gmail.com>
2024-03-04 13:50:16 +0300

    [+] added update task method

--- src/interfaces/task.service.interface.ts
+++ src/interfaces/task.service.interface.ts
@@ -1,8 +1,11 @@
-import { ObjectId } from 'mongoose';
+import mongoose from 'mongoose';
 import { TaskCreateDto } from '../services/dto/task.dto';
 import { ITaskModel } from './task.model.interface';
+import { ITaskUpdate } from './task.update.interface';
 
 export interface ITaskService {
 	createTask: (dto: TaskCreateDto) => Promise<ITaskModel | null>;
 	getAll: (email: string) => Promise<Promise<ITaskModel | null>[]>;
+	// getOne: (id: ObjectId) => Promise<ITaskModel | null>;
+	updateTask: (id: mongoose.Types.ObjectId, data: ITaskUpdate) => Promise<ITaskModel | null>;
 }
