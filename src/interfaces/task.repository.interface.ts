bb2d2003f7bb531d7ac940161536c1f9a9d33b6b
azikreed <azikreed@gmail.com>
2024-03-04 18:52:04 +0300

    [+] added find repository interface

--- src/interfaces/task.repository.interface.ts
+++ src/interfaces/task.repository.interface.ts
@@ -6,7 +6,7 @@ import { ITaskUpdate } from './task.update.interface';
 export interface ITaskRepository {
 	create: (task: Task) => Promise<ITaskModel>;
 	getAll: (userId: ObjectId) => Promise<Promise<ITaskModel | null>[]>;
-	find: (id: ObjectId) => Promise<ITaskModel | null>;
+	find: (id: mongoose.Types.ObjectId) => Promise<ITaskModel | null>;
 	update: (id: mongoose.Types.ObjectId, data: ITaskUpdate) => Promise<ITaskModel | null>;
-	delete: (id: mongoose.Types.ObjectId) => Promise<boolean | null>;
+	delete: (id: mongoose.Types.ObjectId) => Promise<boolean>;
 }
