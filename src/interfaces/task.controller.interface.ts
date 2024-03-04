ce5cd727bc4600704c618213e8be4a373d3cbed0
azikreed <azikreed@gmail.com>
2024-03-04 18:17:08 +0300

    [+] added delete controller interface

--- src/interfaces/task.controller.interface.ts
+++ src/interfaces/task.controller.interface.ts
@@ -4,4 +4,5 @@ export interface ITaskController {
 	create: (req: Request, res: Response, next: NextFunction) => void;
 	getAll: (req: Request, res: Response, next: NextFunction) => void;
 	update: (req: Request, res: Response, next: NextFunction) => void;
+	delete: (req: Request, res: Response, next: NextFunction) => void;
 }
